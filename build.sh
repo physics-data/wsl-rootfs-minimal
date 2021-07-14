375fa7aba9a342340b33820cfd6b7d598f77b3bc
Liu Xiaoyi <circuitcoder0@gmail.com>
2021-07-15 02:57:22 +0800

    GH Actions?

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+EXTRA=$(awk '{print $1}' packages.txt | paste -s -d, -)
+
+echo "Extra packages: $EXTRA"
+
+debootstrap --include=$EXTRA focal ./rootfs
+
+cat > ./generated/etc/apt/sources.list << EOF
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu focal main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu focal-backports main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu focal-security main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu focal-updates main restricted universe multiverse
+EOF
+
+tar -cf ./rootfs.tar -C ./rootfs .
+
+sha256sum rootfs.tar > rootfs.tar.sha256
