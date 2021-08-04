a7bd4c73952f72c692ce38f4edb717ec56ff7adf
Berrysoft <Strawberry_Str@hotmail.com>
2021-08-04 22:37:13 +0800

    Add checks for arch, winver and wsl.exe

--- install.ps1
+++ install.ps1
@@ -6,34 +6,64 @@ function Test-Admin {
     $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 
-if ((Test-Admin) -eq $false)  {
+if (-not (Test-Admin)) {
     if ($elevated) {
         # tried to elevate, did not work, aborting
         echo "Failed to run with elevated privilege. Please install WSL manually."
-    } else {
+    }
+    else {
         Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
     }
     exit
 }
 
 $ErrorActionPreference = "Stop"
-echo "> Enabling WSL feature..."
 
-dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+if ($env:PROCESSOR_ARCHITECTURE -ne "AMD64") {
+    echo "> Only x86_64 system is supported."
+    pause
+    exit 1
+}
 
-$ProgressPreference = 'SilentlyContinue'
+$wininfo = Get-ComputerInfo
 
-echo "> Downloading rootfs..."
+$winversion = [int]$wininfo.OsBuildNumber
 
-Invoke-WebRequest -uri https://meow.plus/physics-data/wsl.tar.zip -OutFile C:\wsl.tar.zip
+if ($winversion -lt 14393) {
+    echo "> Your system is too old. Please update to a newer Windows 10/11."
+    pause
+    exit 1
+}
 
-echo "> Importing rootfs..."
+$wslversion = 1
+if ($winversion -ge 18362) {
+    $wslversion = 2
+}
 
-Expand-Archive -Path C:\wsl.tar.zip -DestinationPath C:\physics-data-wsl
-del C:\wsl.tar.zip
+if (Get-Command "wsl.exe" -ErrorAction SilentlyContinue) {
+    $ProgressPreference = 'SilentlyContinue'
 
-wsl.exe --import physics-data-wsl C:\physics-data-wsl C:\physics-data-wsl\rootfs.tar --version 2
-del C:\physics-data-wsl\rootfs.tar
+    echo "> Downloading rootfs..."
 
-echo "> Installation completed. Please use Windows Terminal to access WSL distro"
-echo "> Alternatively, run 'wsl.exe -d physics-data-wsl'"
+    Invoke-WebRequest -uri https://meow.plus/physics-data/wsl.tar.zip -OutFile C:\wsl.tar.zip
+
+    echo "> Importing rootfs..."
+
+    Expand-Archive -Path C:\wsl.tar.zip -DestinationPath C:\physics-data-wsl
+    del C:\wsl.tar.zip
+
+    wsl.exe --import physics-data-wsl C:\physics-data-wsl C:\physics-data-wsl\rootfs.tar --version $wslversion
+    del C:\physics-data-wsl\rootfs.tar
+
+    echo "> Installation completed. Please use Windows Terminal to access WSL distro"
+    echo "> Alternatively, run 'wsl.exe -d physics-data-wsl'"
+}
+else {
+    echo "> wsl.exe not found. Enabling WSL feature..."
+
+    dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+
+    echo "> WSL enabled. Please restart computer manually, and execute this script again."
+}
+
+pause
