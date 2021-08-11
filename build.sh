821602371cb0609c88aee226049ce146a3966c14
Liu Xiaoyi <circuitcoder0@gmail.com>
2021-08-11 20:07:18 +0800

    Fixed bullseye-security repo

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ debootstrap --include=$EXTRA --exclude=$EXCLUDED bullseye ./rootfs
 cat > ./rootfs/etc/apt/sources.list << EOF
 deb https://mirrors.tuna.tsinghua.edu.cn/debian bullseye main contrib non-free
 deb https://mirrors.tuna.tsinghua.edu.cn/debian bullseye-backports main contrib non-free
-deb https://mirrors.tuna.tsinghua.edu.cn/debian bullseye-security main contrib non-free
+deb https://mirrors.tuna.tsinghua.edu.cn/debian-security bullseye-security main
 deb https://mirrors.tuna.tsinghua.edu.cn/debian bullseye-updates main contrib non-free
 EOF
 
