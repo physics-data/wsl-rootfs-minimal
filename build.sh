09a970cec021ce5f540922ce7acdf57301693354
Liu Xiaoyi <circuitcoder0@gmail.com>
2021-08-11 18:41:47 +0800

    Updated rootfs

--- build.sh
+++ build.sh
@@ -33,31 +33,6 @@ pip3 config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip3 install wheel matplotlib h5py pandas scipy numpy pillow
 EOF
 
-cat >> ./rootfs/home/debian/.bashrc << EOF
-SSH_ENV="$HOME/.ssh/agent-environment"
-
-function start_agent {
-    echo "Initialising new SSH agent..."
-    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-    echo succeeded
-    chmod 600 "${SSH_ENV}"
-    . "${SSH_ENV}" > /dev/null
-    /usr/bin/ssh-add;
-}
-
-# Source SSH settings, if applicable
-
-if [ -f "${SSH_ENV}" ]; then
-    . "${SSH_ENV}" > /dev/null
-    #ps ${SSH_AGENT_PID} doesn't work under cywgin
-    ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-        start_agent;
-    }
-else
-    start_agent;
-fi
-EOF
-
 rm -rf ./rootfs/var/cache/*
 
 tar -cf ./rootfs.tar -C ./rootfs .
